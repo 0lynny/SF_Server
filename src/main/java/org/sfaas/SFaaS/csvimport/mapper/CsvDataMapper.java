27f4c49c82859121c60fa846d53a2edcd43b3905
idealflower-k <aekrcu@naver.com>
2024-12-08 15:32:30 +0900

    [Feat] #10 - DtoToEntity 기능 추가

--- src/main/java/org/sfaas/SFaaS/csvimport/mapper/CsvDataMapper.java
+++ src/main/java/org/sfaas/SFaaS/csvimport/mapper/CsvDataMapper.java
@@ -1,7 +1,10 @@
 package org.sfaas.SFaaS.csvimport.mapper;
 
 import org.sfaas.SFaaS.csvimport.dto.CsvDataDto;
+import org.sfaas.SFaaS.domain.sensor.model.Sensor;
+import org.sfaas.SFaaS.domain.sensorData.model.SensorData;
 
 public interface CsvDataMapper {
 	CsvDataDto csvToDto(String[] csvLine);
+	SensorData DtoToEntity(CsvDataDto csvDto, Sensor sensor);
 }
