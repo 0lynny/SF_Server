f999574b5de752dc40cd6db94f7bdaa501ca70e5
idealflower-k <aekrcu@naver.com>
2024-12-08 12:29:04 +0900

    [Feat] #10 - WeldingCsvConstants 추가

---
+++ src/main/java/org/sfaas/SFaaS/global/common/constants/WeldingCsvConstants.java
@@ -0,0 +1,7 @@
+package org.sfaas.SFaaS.global.common.constants;
+
+import java.time.format.DateTimeFormatter;
+
+public class WeldingCsvConstants {
+	public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+}
