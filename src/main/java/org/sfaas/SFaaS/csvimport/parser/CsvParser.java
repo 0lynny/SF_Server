ac1adc02849939f7622c051556d6c2497fea6546
idealflower-k <aekrcu@naver.com>
2024-12-08 12:28:02 +0900

    [Refactor] #10 - CsvParser 함수 매개변수 으로 변경

--- src/main/java/org/sfaas/SFaaS/csvimport/parser/CsvParser.java
+++ src/main/java/org/sfaas/SFaaS/csvimport/parser/CsvParser.java
@@ -10,17 +10,17 @@ public interface CsvParser {
 
 	/**
 	 * CSV 파일의 전체 내용을 파싱합니다.
-	 * @param inputStream CSV 파일의 InputStream
+	 * @param lines CSV 파일의 lines
 	 * @return 파싱된 데이터의 리스트
 	 * @throws CsvParseException 파싱 중 오류 발생 시
 	 */
-	List<CsvDataDto> parse(InputStream inputStream) throws CsvParseException;
+	List<CsvDataDto> parse(List<String> lines) throws CsvParseException;
 
 	/**
 	 * CSV 파일의 헤더를 파싱합니다.
-	 * @param inputStream CSV 파일의 InputStream
+	 * @param line CSV 파일의 line
 	 * @return 파싱된 헤더 정보
 	 * @throws CsvParseException 파싱 중 오류 발생 시
 	 */
-	List<String> parseHeader(InputStream inputStream) throws CsvParseException;
+	List<String> parseHeader(String line) throws CsvParseException;
 }
